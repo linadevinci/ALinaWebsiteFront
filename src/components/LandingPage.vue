7c6fc99750f2f235814135578faa3b129100e965
alineuh <ton_email_utilisateur>
2025-04-13 22:47:50 +0200

    Fix: ajout ou retrait de LandingPage.vue

---
+++ src/components/LandingPage.vue
@@ -0,0 +1,16 @@
+<template>
+    <div class="landing">
+      <h1>Bienvenue sur le site 🎉</h1>
+      <p>Connecte-toi pour voir une citation !</p>
+    </div>
+  </template>
+  
+  <script setup></script>
+  
+  <style scoped>
+  .landing {
+    text-align: center;
+    margin-top: 2rem;
+  }
+  </style>
+  
