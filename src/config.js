ec8bf4be37f408639d4db8e8c947a9b1019c65de
linadevinci <lina.yahiaoui@edu.devinci.fr>
2025-04-22 12:09:32 +0200

    Ajout de la connexion MongoDB

--- src/config.js
+++ src/config.js
@@ -3,10 +3,10 @@
 // }
 
 export default {
-    apiBaseURL: 'https://alinawebsite-backend-1.onrender.com',
-    endpoints: {
+  apiBaseURL: 'https://alinawebsite-backend-1.onrender.com',
+  endpoints: {
       signup: '/api/users',
       signin: '/api/token',
       quote: '/api/quote'
-    }
   }
+}
